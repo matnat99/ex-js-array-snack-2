62249389f8074ce3826c6c61ccfe7e76ef74c3a7
Mattia Natale <mattianatale07@gmail.com>
2025-06-07 17:05:15 +0200

    feat: add snack 5

--- script.js
+++ script.js
@@ -124,3 +124,25 @@ const agesSum = ages.reduce((acc, age) => acc + age, 0);
 const averageAge = agesSum / ages.length;
 
 console.log(averageAge);
+
+/*
+Snack 5 (Bonus) - Raccogli i libri
+
+Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(),
+per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+Testala con l’array [2, 13, 7, 21, 19] .
+*/ 
+
+const ids = [2, 13, 7, 21, 19] 
+
+async function getBooks(ids) {
+const url = `http://localhost:3333/books/`
+
+const bookPromises = ids.map(id => fetch(`${url}${id}`).then(res => res.json()))
+
+const books = await Promise.all(bookPromises)
+
+return books 
+}
+
+getBooks(ids).then(books => console.log(books))
